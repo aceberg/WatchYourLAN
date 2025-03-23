022f266038c6d070eee4681048edc7df2c5166ac
aceberg <1502200+aceberg@users.noreply.github.com>
2025-03-23 14:23:21 +0700

    Vite fixed file names

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,13 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
